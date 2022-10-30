a5f12ffa9ae9320e84f670d85018e4d2bf2ccdf0
netopir <neto_pir@hotmail.com>
2022-10-30 07:04:38 +0100

    Update eac3to_log.bat

--- eac3to_log.bat
+++ eac3to_log.bat
@@ -11,29 +11,42 @@ REM
 echo [95m"Play List"
 set /p playlist=" "[0m
 REM
-echo [90m"Create @eac3to_log"[0m
-%eac3todirectory% %source% %playlist%) -log=%source%\@eac3to_log.txt
+echo [94m"Destination Path"
+set /p destination=" "[0m
 REM
-IF EXIST %source%\@remux.txt DEL /F %source%\@remux.txt
+md %source%\"%destination%"
+REM
+echo [90m"Create @eac3to_log"[0m
+%eac3todirectory% %source% %playlist%) -log=%source%\%destination%\@eac3to_log.txt
+REM
+IF EXIST %source%\%destination%\@remux.txt DEL /F %source%\%destination%\@remux.txt
+REM
+timeout /t 2 /nobreak > nul
 REM
 echo [90m"Create @remux"[0m
-powershell -f C:\BRtools\eac3to\log2cmd_v1.ps1 %source%\@eac3to_log.txt %source%\@remux.txt
+powershell -f C:\BRtools\eac3to\log2cmd_v1.ps1 %source%\%destination%\@eac3to_log.txt %source%\%destination%\@remux.txt
 REM
-powershell -f C:\BRtools\eac3to\log2cmd_v1.ps1 %source%\@eac3to_log.txt %source%\@remux_all.txt
+timeout /t 2 /nobreak > nul
+REM
+powershell -f C:\BRtools\eac3to\log2cmd_v1.ps1 %source%\%destination%\@eac3to_log.txt %source%\%destination%\@remux_all.txt
 REM
 echo [90m"Open @remux"[0m
-powershell notepad.exe %source%\@remux.txt
+powershell notepad.exe %source%\%destination%\@remux.txt
 REM
 pause
 echo [90m"Remove spaces @remux"[0m
-for /f "usebackq tokens=* delims=" %%a in ("%source%\@remux.txt") do (echo(%%a)>>~.txt
-move /y  ~.txt "%source%\@remux.txt"
+for /f "usebackq tokens=* delims=" %%a in ("%source%\%destination%\@remux.txt") do (echo(%%a)>>~.txt
+move /y  ~.txt "%source%\%destination%\@remux.txt"
 REM
-for /f "usebackq tokens=* delims=" %%a in ("%source%\@remux_all.txt") do (echo(%%a)>>~.txt
-move /y  ~.txt "%source%\@remux_all.txt"
+timeout /t 2 /nobreak > nul
+REM
+for /f "usebackq tokens=* delims=" %%a in ("%source%\%destination%\@remux_all.txt") do (echo(%%a)>>~.txt
+move /y  ~.txt "%source%\%destination%\@remux_all.txt"
+REM
+timeout /t 2 /nobreak > nul
 REM
 echo [90m"Run @remux"[0m
-cmd < %source%\@remux.txt
+cmd < "%source%"\"%destination%"\@remux.txt
 REM
 pause
 REM
