f1e642b92ce1a04ba3b49e1156fab5ab3491056a
netopir <neto_pir@hotmail.com>
2022-11-10 19:22:13 +0100

    Delete List_all_files.ps1

--- List_all_files.ps1
+++
@@ -1,2 +0,0 @@
-param([string]$source, [string]$destination, [string]$archivename)
-Get-ChildItem -Recurse $source | Where { ! $_.PSIsContainer } | Select Name, @{Name="MBytes";Expression={ "{0:N2}" -f ($_.Length / 1MB) }} | Out-File -FilePath $destination\$archivename.nfo
