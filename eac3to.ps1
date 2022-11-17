d5bad868be7362753ae0d50d16f283f124c37858
netopir <neto_pir@hotmail.com>
2022-11-17 09:53:35 +0100

    Update eac3to.ps1

--- eac3to.ps1
+++ eac3to.ps1
@@ -1,30 +1,33 @@
 #PowerShell: Set the location to eac3to programm
 $eac3toPath = "C:\BRtools\eac3to\eac3to.exe"
 #PowerShell: Set the directory of the Source Path
-$source = Read-Host -Prompt "Enter Source Path"
+$source = Read-Host -Prompt (Write-Color "Enter Source Path: " -Color DarkMagenta -NoNewLine)
+#$source = Read-Host -Prompt "Enter Source Path"
 #PowerShell: scan the BluRay directory
-Write-host "Reading BluRay" -ForegroundColor Gray
+Write-Host "Reading BluRay" -ForegroundColor DarkCyan
 & cmd @('/c', $eac3toPath, $source )
 #PowerShell: enter the playlist number from the scaned BluRay
-$playlist = Read-Host -Prompt "Enter Play List"
+$playlist = Read-Host -Prompt (Write-Color "Enter Play List: " -Color DarkMagenta -NoNewLine)
+#$playlist = Read-Host -Prompt "Enter Play List"
 #PowerShell: result from the choisen Playlist
-Write-host "Content of BluRay" -ForegroundColor Gray
-& cmd @('/c', $eac3toPath, $source, $playlist )
+Write-Host "Content of BluRay" -ForegroundColor DarkCyan
+& cmd @('/c', $eac3toPath, $source, "$($playlist))" )
 #PowerShell: Set the directory of the destination Path
-$destination = Read-Host -Prompt "Enter Destination Path"
+$destination = Read-Host -Prompt (Write-Color "Enter Destination Path: " -Color DarkMagenta -NoNewLine)
+#$destination = Read-Host -Prompt "Enter Destination Path"
 #PowerShell: Pause for 2 seconds
 timeout /t 2 | Out-Null
 #PowerShell: Get-Item cmdlet to check if folder exists or not and create directory if not exists.
 if(Get-Item -Path $destination -ErrorAction Ignore)
 {
-    Write-Host "Folder Exists" -ForegroundColor Gray
+    Write-Host "Folder Exists" -ForegroundColor DarkRed
 }
 else
 {
-    Write-Host "Folder Doesn't Exists" -ForegroundColor Gray
+    Write-Host "Folder Doesn't Exists" -ForegroundColor DarkYellow
     #PowerShell: Create directory if not exists
     New-Item $destination -ItemType Directory | Out-Null
-    Write-Host "Folder Created successfully" -ForegroundColor Gray
+    Write-Host "Folder Created successfully" -ForegroundColor DarkGreen
 }
 #PowerShell: Pause for 2 seconds
 timeout /t 2 | Out-Null
@@ -33,14 +36,14 @@ $logfile = "-log=$destination\@eac3to_log.txt"
 #PowerShell: Chek if logfile exist then delite it
 if (Test-Path $logfile) {
   Remove-Item -Verbose -Force $logfile
-  write-host "$logfile has been deleted" -ForegroundColor Gray
+  write-Host "$logfile has been deleted" -ForegroundColor DarkRed
 }
 else {
-  Write-host "$logfile doesn't exist" -ForegroundColor Gray
+  Write-Host "$logfile doesn't exist" -ForegroundColor DarkYellow
 }
 #PowerShell: Create @eac3to_log file
-Write-host "Create @eac3to_log" -ForegroundColor Gray
-& cmd @('/c', $eac3toPath, $source, $playlist, $logfile )
+Write-Host "Create @eac3to_log" -ForegroundColor DarkYellow
+& cmd @('/c', $eac3toPath, $source, "$($playlist))", $logfile )
 #PowerShell: Pause for 2 seconds
 timeout /t 2 | Out-Null
 #PowerShell: Set the @remux file
@@ -48,10 +51,10 @@ $FileNameremux = "$destination\@remux.txt"
 #PowerShell: Chek if FileNameremux exist then delite it
 if (Test-Path $FileNameremux) {
   Remove-Item -Verbose -Force $FileNameremux
-  write-host "$FileNameremux has been deleted" -ForegroundColor Gray
+  write-Host "$FileNameremux has been deleted" -ForegroundColor DarkRed
 }
 else {
-  Write-host "$FileNameremux doesn't exist" -ForegroundColor Gray
+  Write-Host "$FileNameremux doesn't exist" -ForegroundColor DarkYellow
 }
 #PowerShell: Pause for 2 seconds
 timeout /t 2 | Out-Null
@@ -60,26 +63,26 @@ $FileNameremuxall = "$destination\@remux_all.txt"
 #PowerShell: chek if FileNameremuxall exist then delite it
 if (Test-Path $FileNameremuxall) {
   Remove-Item -Verbose -Force $FileNameremuxall
-  write-host "$FileNameremuxall has been deleted" -ForegroundColor Gray
+  write-Host "$FileNameremuxall has been deleted" -ForegroundColor DarkRed
 }
 else {
-  Write-host "$FileNameremuxall doesn't exist" -ForegroundColor Gray
+  Write-Host "$FileNameremuxall doesn't exist" -ForegroundColor DarkYellow
 }
 #PowerShell: Pause for 2 seconds
 timeout /t 2 | Out-Null
 #PowerShell: Create @remux file
-Write-host "Create @remux" -ForegroundColor Gray
-powershell -f C:\BRtools\eac3to\log2cmd_v1.ps1 "$destination\@eac3to_log.txt" "$destination\@remux.txt"
+Write-Host "Create @remux" -ForegroundColor DarkCyan
+powershell -f C:\BRtools\eac3to\log2cmd_v1.ps1 "$destination\@eac3to_log.txt" "$destination\@remux.txt" | Out-Null
 #PowerShell: Pause for 2 seconds
 timeout /t 2 | Out-Null
 #PowerShell: Create @remux_all file
-Write-host "Create @remux_all" -ForegroundColor Gray
+Write-Host "Create @remux_all" -ForegroundColor DarkCyan
 powershell -f C:\BRtools\eac3to\log2cmd_v1.ps1 "$destination\@eac3to_log.txt" "$destination\@remux_all.txt" | Out-Null
 #PowerShell: Open @remux with notepad
-Write-host "Open @remux" -ForegroundColor Gray
+Write-Host "Open @remux" -ForegroundColor DarkCyan
 powershell notepad.exe "$destination\@remux.txt"
 #PowerShell: Press any key to continue . . .
-read-host "Press ENTER to continue..."
+Read-Host (Write-Color "Press ENTER to continue..." -Color DarkGray -NoNewLine):
 #PowerShell: Remove blank lines from a file
 (Get-Content "$destination\@remux.txt") | Where-Object {$_.trim() -ne "" } | Set-Content "$destination\@remux.txt"
 #PowerShell: Pause for 2 seconds
@@ -88,7 +91,19 @@ timeout /t 2 | Out-Null
 (Get-Content "$destination\@remux_all.txt") | Where-Object {$_.trim() -ne "" } | Set-Content "$destination\@remux_all.txt"
 #PowerShell: Pause for 2 seconds
 timeout /t 2 | Out-Null
+#PowerShell: Copy a file to the specified directory and rename the file
+Copy-Item "$destination\@remux.txt" -Destination "C:\BRtools\eac3to\@remux.bat"
+#PowerShell: Pause for 2 seconds
+timeout /t 2 | Out-Null
 #PowerShell: run the remux
-# ???
+Start-Process -FilePath "C:\BRtools\eac3to\@remux.bat" RunAs -Wait -NoNewWindow
+#PowerShell: Pause for 2 seconds
+timeout /t 2 | Out-Null
+#PowerShell: Delete file hidden, read-only files
+Remove-Item -Path "C:\BRtools\eac3to\@remux.bat" -Force
+#PowerShell: Pause for 2 seconds
+timeout /t 2 | Out-Null
+#PowerShell: Open all log files
+#Invoke-Item "$destination\*.txt"
 #PowerShell: Press any key to continue . . .
-read-host "Press ENTER to continue..."
+Read-Host (Write-Color "Press ENTER to continue..." -Color DarkGray -NoNewLine):
